4b35a03bf9cef257d2c7f9ab91c2564711b5ac8d
Dylan Crawshaw <dcrawsh@gmail.com>
2021-01-29 10:05:47 -0800

    redo styling

--- app/javascript/packs/Eventlite.js
+++ app/javascript/packs/Eventlite.js
@@ -115,7 +115,7 @@ class Eventlite extends React.Component {
   render() {
     return (
       <div>
-        <h1 className="logo" ref={this.logo}>Eventlite</h1>
+        <h1 className="logo" ref={this.logo}>Portland Eventer</h1>
         <FormErrors formErrors = {this.state.formErrors} />
         <EventForm handleSubmit = {this.handleSubmit}
           handleInput = {this.handleInput}
